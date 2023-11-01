09e4fd6524d022780810b7b9133db9ff05e729e5
Hüseyin Çinibulak <huseyincnblk@gmail.com>
2023-11-01 14:31:33 +0300

    fragment_add_note.xml and fragment_main.xml completed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,10 @@ android {
     namespace = "com.hsyncnblk.noteappsqlite"
     compileSdk = 33
 
+    buildFeatures{
+        viewBinding = true
+    }
+
     defaultConfig {
         applicationId = "com.hsyncnblk.noteappsqlite"
         minSdk = 29
@@ -44,4 +48,6 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation ("androidx.navigation:navigation-fragment-ktx:2.7.3")
+    implementation ("androidx.navigation:navigation-ui-ktx:2.7.3")
 }
---
+++ app/src/main/java/com/hsyncnblk/noteappsqlite/fragment/AddNoteFragment.kt
@@ -0,0 +1,23 @@
+package com.hsyncnblk.noteappsqlite.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.hsyncnblk.noteappsqlite.R
+
+
+class AddNoteFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_note, container, false)
+    }
+
+
+}
